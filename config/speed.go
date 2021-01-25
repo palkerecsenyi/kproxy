a4bed9bbb2ebdf041fd6b5584ac00305dbb74f5f
Pal Kerecsenyi <palkerecs@gmail.com>
2021-01-25 09:06:47 +0000

    Improve size measuring

--- config/speed.go
+++ config/speed.go
@@ -12,19 +12,21 @@ func getSpeedTestPage(res http.ResponseWriter, _ *http.Request) {
 
 func startSpeedTest(res http.ResponseWriter, req *http.Request) {
 	megabytesString := req.URL.Query().Get("mb")
-	megabytes, err := strconv.Atoi(megabytesString)
+	megabytes, err := strconv.ParseFloat(megabytesString, 64)
 	if megabytes <= 0 || megabytes > 500 || err != nil {
 		res.WriteHeader(400)
 		return
 	}
 
-	randomData := make([]byte, megabytes*1000000)
+	bytes := int(megabytes * 1000000)
+	randomData := make([]byte, bytes)
 	_, err = rand.Read(randomData)
 	if err != nil {
 		res.WriteHeader(500)
 		return
 	}
 
+	res.Header().Add("Content-Length", strconv.Itoa(bytes))
 	res.Header().Add("Cache-Control", "no-cache, no-store, private")
 	_, _ = res.Write(randomData)
 
